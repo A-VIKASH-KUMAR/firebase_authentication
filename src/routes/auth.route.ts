39a6d2625998bcf0024bd1b002d8da401de436d5
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-02-26 23:34:27 +0530

    feat: :sparkles: add auth google-user to get google user details

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, googlelogin } from "../controllers/auth.controller";
+import { register, login, googlelogin, googleUser } from "../controllers/auth.controller";
 import { validateRegister, validateLogin } from "../middlewares/auth.validate";
 
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/register", validateRegister, register);
 router.post("/login", validateLogin, login);
 router.get("/google", googlelogin);
-router.post("/google-user")
+router.post("/google-user", googleUser)
 
 export default router;
