67fe4d12a76ec0647da4d9d94dc4d5085e923042
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-02-27 21:00:35 +0530

    feat: :sparkles: remove mongodb connect from index file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from "express";
-import authRoute from "./routes/auth.route"
+import authRoute from "./routes/auth.route";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-
+import swaggerRoute from "./utils/swagger";
 dotenv.config();
 const app = express();
-import { connectDb } from "./db";
-// import { isAuthenticate } from "./middlewares/isAuthenticate";
 const port = 3001;
 app.use(bodyParser.json());
 
@@ -14,14 +12,11 @@ app.get("/", function (req: any, res: any) {
   res.send("Hello World");
 });
 
-
-connectDb().then(() => {
-  app.listen(port, () => {
-    
-    console.log("server is running on port http://localhost:" + port);
-  });
+app.listen(port, () => {
+  console.log("server is running on port http://localhost:" + port);
 });
 
 app.use(express.json());
-app.use("/api/auth",  authRoute);
+app.use("/api/auth", authRoute);
+app.use("/api-docs", swaggerRoute);
 // app.use("/api", isAuthenticate, todoRoute);
