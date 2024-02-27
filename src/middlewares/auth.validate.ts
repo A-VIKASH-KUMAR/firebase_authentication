bd959ae34976a199d8c99867d57ff57ec8bb8863
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-02-27 21:02:45 +0530

    feat: :sparkles: auth validate update

--- src/middlewares/auth.validate.ts
+++ src/middlewares/auth.validate.ts
@@ -1,7 +1,7 @@
 export const validateRegister = (req:any, res:any, next:any) => {
     const {email, password} = req.body;
 
-    if (!email || password || email === "" ||password === "") {
+    if (!email || !password || email === "" ||password === "") {
         return res.status(403).json({error:"Please provide email and password to register"})
     }
     return next()
